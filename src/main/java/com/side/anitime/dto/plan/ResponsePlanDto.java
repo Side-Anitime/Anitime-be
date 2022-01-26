c79fb71aeca47f78e9765956b504dfb35afa5773
bomingeem <bomingeem@gmail.com>
2022-01-27 08:56:59 +0900

    코멘트 추가

--- src/main/java/com/side/anitime/dto/plan/ResponsePlanDto.java
+++ src/main/java/com/side/anitime/dto/plan/ResponsePlanDto.java
@@ -20,7 +20,7 @@ public class ResponsePlanDto {
         private Long planId;
         private String title;
         private String contents;
-        private PlanCategory planCategory;
+        private PlanCategory planCategory; //TODO: 이녀석도 아래와 같이 마찬가지입니다
         private List<Alarm> alarms; //TODO: Dto 통신 시 다이렉트로 domain을 받아선 안됩니다~ Dto는 단순 데이터에서 조회온 값을 필요한 것 또는 가공을 위한 것이므로 Alarm에 대한 Dto를 생성해서 받아야 함
         // TODO 단건 조회할 때 일정에 등록된 하나의 알람정보는 어떻게 들고오지..?
     }
