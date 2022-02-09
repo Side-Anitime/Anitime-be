5407fcd550a9e9253dd5fdee07e892717ed94ba2
bomingeem <bomingeem@gmail.com>
2022-02-09 23:08:11 +0900

    swagger 테스트

--- src/main/java/com/side/anitime/controller/alarm/AlarmController.java
+++ src/main/java/com/side/anitime/controller/alarm/AlarmController.java
@@ -3,20 +3,32 @@ package com.side.anitime.controller.alarm;
 import com.side.anitime.domain.alarm.Alarm;
 import com.side.anitime.dto.alarm.ResponseAlarmDto;
 import com.side.anitime.repository.alarm.AlarmRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/alarm")
 @RequiredArgsConstructor
+@Api(value = "알람 API", tags = "알람 API")
 public class AlarmController {
 
     private final AlarmRepository alarmRepository;
 
+    @ApiOperation(value = "알람 목록 조회")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully get alarm list", response = ResponseAlarmDto.class
+            )
+    })
     @GetMapping("{alarmId}")
     public ResponseEntity<?> getAlarmList(@PathVariable Long alarmId) {
-        Alarm alarm = alarmRepository.findById(alarmId).orElseThrow(() -> new IllegalStateException("Error~"));
+        Alarm alarm = alarmRepository.findById(alarmId).orElseThrow(() -> new IllegalStateException("NotFoundException 클래스 생성 필요"));
 
         return ResponseEntity.ok(
                 ResponseAlarmDto.Detail.builder()
