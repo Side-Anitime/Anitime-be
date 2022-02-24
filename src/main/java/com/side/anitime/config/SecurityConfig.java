635d0fe2e3b64bb43a21cc3ae217e17903c834f2
kymkyj <ymyj33@naver.com>
2022-02-24 23:24:28 +0900

    [feat] kakao 의존성 설정 yml 추가

---
+++ src/main/java/com/side/anitime/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.side.anitime.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // #TODO 시큐리티 내용 추가 예정
+}
