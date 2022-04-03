4b82231de847c256a5a2cf16af44528f1bde88af
bomingeem <bomingeem@gmail.com>
2022-04-03 16:10:30 +0900

    일반 회원가입 로직 임시커밋

--- src/main/java/com/side/anitime/service/UserService.java
+++ src/main/java/com/side/anitime/service/UserService.java
@@ -1,6 +1,8 @@
 package com.side.anitime.service;
 
+import com.side.anitime.codeconst.UserType;
 import com.side.anitime.dto.RequestUserDto;
+import com.side.anitime.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
     public void joinUser(RequestUserDto.Join request) {
         //TODO:
-        // 1. userType 확인
-        // 2. 이메일 중복체크
+        // 1. userType 확인 (O)
+        // 2. 이메일 중복체크 (O)
         // 3. 사용자 기본정보 저장
         // 4. save
+        if (request.getUserType().equals(UserType.NORMAL)) {
+            if (userRepository.countUserByEmail(request.getEmail()) > 0) {
+                //exception 처리
+            }
+        }
     }
 }
